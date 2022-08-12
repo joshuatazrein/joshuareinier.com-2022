01c0c74c5a09d11ccfede7f0a0f3169185ba8a0e
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2022-08-11 20:06:07 -0400

    fixed scores

--- components/ScoreCover.js
+++ components/ScoreCover.js
@@ -1,34 +1,13 @@
 import { useEffect, useRef } from "react";
 
 export default function ScoreCover(props) {
-  const thisImg = useRef();
-  const thisDiv = useRef();
-
-  useEffect(() => {
-    console.log($(thisDiv.current).height());
-    $(thisImg.current).attr(
-      "width",
-      $(thisDiv.current).height() *
-        ($(thisImg.current).width() / $(thisImg.current).height())
-    );
-  });
-
   return (
-    <div
-      ref={thisDiv}
+    <img
+      src={props.src}
+      className="m-1 shrink h-full overflow-hidden ml-auto j-shadow-800 bg-semiblack-800 max-h-full object-contain"
       style={{
         maxWidth: "calc(100% - 100px)",
       }}
-      className={
-        "m-1 ml-auto j-shadow-800 bg-semiblack-800 shrink h-full overflow-hidden " +
-        (props.className || "")
-      }
-    >
-      <img
-        ref={thisImg}
-        src={props.src}
-        className="max-h-full max-w-full"
-      ></img>
-    </div>
+    ></img>
   );
 }
